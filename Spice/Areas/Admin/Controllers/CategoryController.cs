79085fa13825c7cb7dd1d2f07ff24960b34887ee
Sachin Kumar <sachinengineer@hotmail.com>
2020-02-09 02:16:13 +0530

    Added Category Menu item

---
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category categaory)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(categaory);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Category.Update(category);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        //POST DELETE
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var itemtobedeleted = await _db.Category.FindAsync(id);
+            if (itemtobedeleted == null)
+                return NotFound();
+            _db.Category.Remove(itemtobedeleted);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var categoryEdited = await _db.Category.FindAsync(id);
+            if(categoryEdited==null)
+            {
+                return NotFound();
+            }
+            return View(categoryEdited);
+        }
+
+        //GET DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var categoryEdited = await _db.Category.FindAsync(id);
+            if (categoryEdited == null)
+            {
+                return NotFound();
+            }
+            return View(categoryEdited);
+        }
+
+        //GET DETAILS
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var categoryEdited = await _db.Category.FindAsync(id);
+            if (categoryEdited == null)
+            {
+                return NotFound();
+            }
+            return View(categoryEdited);
+        }
+    }
+}
